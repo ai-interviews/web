5400e694ed39e67b2523803aac75e64ce94f60f1
Lina <lina.moufarreh@gmail.com>
2023-06-29 18:32:14 -0400

    profile page complete

--- app/(app)/profile/_components/ProfileInfo.tsx
+++ app/(app)/profile/_components/ProfileInfo.tsx
@@ -1,13 +1,14 @@
 import { getServerUser } from '@/app/_lib/server/getServerUser';
-import React from 'react';
 
-export async function ProfilePage() {
+
+export async function ProfileInfo() {
   const user = await getServerUser();
+
 
   return (
     <div className="flex flex-col gap-5">
       <div className="flex flex-col gap-5">
-        <div className="text-2xl">Profile</div>
+        <div className="text-2xl">My information</div>
         <div className="flex flex-col gap-2">
           <div className="text-xl">Name</div>
           <div className="text-sm opacity-50">{user.name}</div>
@@ -17,14 +18,19 @@ export async function ProfilePage() {
           <div className="text-sm opacity-50">{user.email}</div>
         </div>  
         <div className="flex flex-col gap-2">
-          <div className="text-xl">LinkedIn URL</div>
+          <div className="text-xl">Country</div>  
+          <div className="text-sm opacity-50">{user.country}</div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="text-xl">LinkedIn Profile</div>
           <div className="text-sm opacity-50">{user.linkedIn}</div>
         </div>
         <div className="flex flex-col gap-2">
-          <div className="text-xl">Country</div>  
-          <div className="text-sm opacity-50">{user.country}</div>
+          <div className="text-xl">User Since</div>  
+          <div className="text-sm opacity-50">{user.userSince?.toDateString()}</div>
         </div>
       </div>
     </div>
   )
 }
+
