ad802afa45ad63927b27b931f933f3a4eadb44e1
SaraBedd <sara.beddouch@polymtl.ca>
2023-07-18 18:35:01 -0400

    Finalized metrics on dashboard

--- app/(app)/dashboard/_components/DashboardMetrics.tsx
+++ app/(app)/dashboard/_components/DashboardMetrics.tsx
@@ -12,47 +12,15 @@ type Props = {
 export async function DashboardMetrics({ responses }: Props) {
   const chartMetrics = formatChartMetrics({ responses });
 
-  const { timeSeconds, quietTimeSeconds, wordFrequency, slangFrequency } =
-    chartMetrics.responseDatasets;
-
-  const avgQuantifiedMetric =
-    quantifiedMetric.dataset.data.reduce((acc, val) => acc + val, 0) /
-      quantifiedMetric.dataset.data.length || 0;
-
+  const {
+    timeSeconds,
+    quietTimeSeconds,
+    wordFrequency,
+    quantifiedMetric,
+    slangFrequency,
+  } = chartMetrics.responseDatasets;
   return (
     <div className="flex w-full flex-wrap items-center justify-between gap-5">
-      <div className="flex w-full flex-col gap-3 lg:flex-1">
-        <MetricCard
-          title={<>{quietTimeSeconds.avg.toFixed(2)} s</>}
-          subtext={"Average thinking time."}
-        >
-          {quietTimeSeconds.dataset.data.length ? (
-            <BarChart
-              labels={quietTimeSeconds.labels}
-              dataset={quietTimeSeconds.dataset}
-              className="h-24 w-24  2xl:h-full 2xl:w-36"
-              barThickness={6}
-              hideGridLines
-            />
-          ) : (
-            <div className="w-24 text-center text-sm 2xl:w-36">No data</div>
-          )}
-        </MetricCard>
-        <MetricCard
-          title={wordFrequency.dataset.data.length}
-          subtext="Different filler words detected."
-        >
-          {wordFrequency.dataset.data.length ? (
-            <PieChart
-              labels={wordFrequency.labels}
-              dataset={wordFrequency.dataset}
-              className="w-24 2xl:w-36"
-            />
-          ) : (
-            <div className="w-24 text-center text-sm 2xl:w-36">No data</div>
-          )}
-        </MetricCard>
-      </div>
       <Card className="relative flex h-full w-full flex-col justify-center lg:w-96 lg:flex-auto">
         <div className="w-full pb-5 pl-4 text-2xl">Response times</div>
         <BarChart
@@ -70,7 +38,12 @@ export async function DashboardMetrics({ responses }: Props) {
         <div className="flex w-full flex-col gap-3 lg:flex-row">
           <MetricCard
             title={
-              <>{avgQuietTimeSeconds === "0.00" ? 0 : avgQuietTimeSeconds} s</>
+              <>
+                {quietTimeSeconds.avg.toFixed(2) === "0.00"
+                  ? 0
+                  : quietTimeSeconds.avg.toFixed(2)}{" "}
+                s
+              </>
             }
             subtext={"Average thinking time this month."}
           >
@@ -104,7 +77,7 @@ export async function DashboardMetrics({ responses }: Props) {
               </div>
             )}
           </MetricCard>
-          {/* <MetricCard
+          <MetricCard
             title={<></>}
             subtext={
               "Average number of quantifiable metrics detected in your responses this month."
@@ -112,14 +85,14 @@ export async function DashboardMetrics({ responses }: Props) {
           >
             {quantifiedMetric.dataset.data.length ? (
               <div className="w-24 text-center lg:w-1/4 2xl:w-36 2xl:text-5xl">
-                {avgQuantifiedMetric}
+                {quantifiedMetric.avg.toFixed(1)}
               </div>
             ) : (
               <div className="w-24 text-center text-sm lg:w-1/4 2xl:w-36">
                 No data
               </div>
             )}
-          </MetricCard> */}
+          </MetricCard>
           <MetricCard
             title={slangFrequency.labels.length.toString()}
             subtext="Different slang words detected this month."
