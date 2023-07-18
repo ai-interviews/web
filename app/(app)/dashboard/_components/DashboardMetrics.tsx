36ca55a438f05957a24e44b33354fa034d081691
SaraBedd <sara.beddouch@polymtl.ca>
2023-07-18 18:14:16 -0400

    Finalized metrics on dashboard

--- app/(app)/dashboard/_components/DashboardMetrics.tsx
+++ app/(app)/dashboard/_components/DashboardMetrics.tsx
@@ -18,86 +18,20 @@ export async function DashboardMetrics() {
 
   const { wordFrequency, slangFrequency } = chartMetrics.aggregateDatasets;
 
-  const { timeSeconds, quietTimeSeconds, quantifiedMetric } = chartMetrics.responseDatasets;
+  const { timeSeconds, quietTimeSeconds, quantifiedMetric } =
+    chartMetrics.responseDatasets;
 
   const avgQuietTimeSeconds = (
     quietTimeSeconds.dataset.data.reduce((acc, val) => acc + val, 0) /
       quietTimeSeconds.dataset.data.length || 0
   ).toFixed(2);
 
-  const avgQuantifiedMetric = (
+  const avgQuantifiedMetric =
     quantifiedMetric.dataset.data.reduce((acc, val) => acc + val, 0) /
-    quantifiedMetric.dataset.data.length || 0
-  ).toFixed(2);
+      quantifiedMetric.dataset.data.length || 0;
 
   return (
     <div className="flex w-full flex-wrap items-center justify-between gap-5">
-      <div className="flex w-full flex-col gap-3 lg:flex-1">
-        <MetricCard
-          title={
-            <>{avgQuietTimeSeconds === "0.00" ? 0 : avgQuietTimeSeconds} s</>
-          }
-          subtext={"Average thinking time this month."}
-        >
-          {quietTimeSeconds.dataset.data.length ? (
-            <BarChart
-              labels={quietTimeSeconds.dataset.data.map(() => "")}
-              dataset={quietTimeSeconds.dataset}
-              className="h-24 w-24  2xl:h-full 2xl:w-36"
-              barThickness={6}
-              hideGridLines
-            />
-          ) : (
-            <div className="w-24 text-center text-sm 2xl:w-36">No data</div>
-          )}
-        </MetricCard>
-        <MetricCard
-          title={
-            <>{avgQuantifiedMetric === "0.00" ? 0 : avgQuantifiedMetric} s</>
-          }
-          subtext={"Average thinking time this month."}
-        >
-          {quantifiedMetric.dataset.data.length ? (
-            <BarChart
-              labels={quantifiedMetric.dataset.data.map(() => "")}
-              dataset={quantifiedMetric.dataset}
-              className="h-24 w-24  2xl:h-full 2xl:w-36"
-              barThickness={6}
-              hideGridLines
-            />
-          ) : (
-            <div className="w-24 text-center text-sm 2xl:w-36">No data</div>
-          )}
-        </MetricCard>
-        {/* <MetricCard
-          title={slangFrequency.labels.length.toString()}
-          subtext="Different slang words detected this month."
-        >
-          {slangFrequency.dataset.data.length ? (
-            <PieChart
-              labels={slangFrequency.labels}
-              dataset={slangFrequency.dataset}
-              className="w-24 2xl:w-36"
-            />
-          ) : (
-            <div className="w-24 text-center text-sm 2xl:w-36">No data</div>
-          )}
-        </MetricCard> */}
-        <MetricCard
-          title={wordFrequency.dataset.data.length}
-          subtext="Different filler words detected this month."
-        >
-          {wordFrequency.dataset.data.length ? (
-            <PieChart
-              labels={wordFrequency.labels}
-              dataset={wordFrequency.dataset}
-              className="w-24 2xl:w-36"
-            />
-          ) : (
-            <div className="w-24 text-center text-sm 2xl:w-36">No data</div>
-          )}
-        </MetricCard>
-      </div>
       <Card className="relative flex h-full w-full flex-col justify-center lg:w-96 lg:flex-auto">
         <div className="w-full pb-5 pl-4 text-2xl">Response times</div>
         <BarChart
@@ -111,6 +45,78 @@ export async function DashboardMetrics() {
           </div>
         )}
       </Card>
+      <div className="flex w-full flex-wrap items-center justify-between gap-5">
+        <div className="flex w-full flex-col gap-3 lg:flex-row">
+          <MetricCard
+            title={
+              <>{avgQuietTimeSeconds === "0.00" ? 0 : avgQuietTimeSeconds} s</>
+            }
+            subtext={"Average thinking time this month."}
+          >
+            {quietTimeSeconds.dataset.data.length ? (
+              <BarChart
+                labels={quietTimeSeconds.dataset.data.map(() => "")}
+                dataset={quietTimeSeconds.dataset}
+                className="h-24 w-24 lg:w-1/4 2xl:h-full 2xl:w-36"
+                barThickness={6}
+                hideGridLines
+              />
+            ) : (
+              <div className="w-24 text-center text-sm lg:w-1/4 2xl:w-36">
+                No data
+              </div>
+            )}
+          </MetricCard>
+          <MetricCard
+            title={wordFrequency.dataset.data.length}
+            subtext="Different filler words detected this month."
+          >
+            {wordFrequency.dataset.data.length ? (
+              <PieChart
+                labels={wordFrequency.labels}
+                dataset={wordFrequency.dataset}
+                className="w-24 lg:w-1/4 2xl:w-36"
+              />
+            ) : (
+              <div className="w-24 text-center text-sm lg:w-1/4 2xl:w-36">
+                No data
+              </div>
+            )}
+          </MetricCard>
+          <MetricCard
+            title={<></>}
+            subtext={
+              "Average number of quantifiable metrics detected in your responses this month."
+            }
+          >
+            {quantifiedMetric.dataset.data.length ? (
+              <div className="w-24 text-center lg:w-1/4 2xl:w-36 2xl:text-5xl">
+                {avgQuantifiedMetric}
+              </div>
+            ) : (
+              <div className="w-24 text-center text-sm lg:w-1/4 2xl:w-36">
+                No data
+              </div>
+            )}
+          </MetricCard>
+          <MetricCard
+            title={slangFrequency.labels.length.toString()}
+            subtext="Different slang words detected this month."
+          >
+            {slangFrequency.dataset.data.length ? (
+              <PieChart
+                labels={slangFrequency.labels}
+                dataset={slangFrequency.dataset}
+                className="w-24 lg:w-1/4 2xl:w-36"
+              />
+            ) : (
+              <div className="w-24 text-center text-sm lg:w-1/4 2xl:w-36">
+                No data
+              </div>
+            )}
+          </MetricCard>
+        </div>
+      </div>
     </div>
   );
 }
